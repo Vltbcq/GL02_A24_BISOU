28c4e4cc62bc3e3381d0582ac5b8596ce00d4dbd
Elia Gautier <elia.gautier@utt.fr>
2024-11-13 13:10:02 +0100

    Ajout des dernières opérations controller de création de questions et de leurs TU

--- app/controller/QuestionController.js
+++ app/controller/QuestionController.js
@@ -1,6 +1,9 @@
 const BlankWordQuestion = require('../model/base-types/implementations/BlankWordQuestion')
 const QuestionCache = require('./utils/QuestionCache')
 const MultipleChoiceQuestion = require('../model/base-types/implementations/MultipleChoiceQuestion')
+const NumericQuestion = require('../model/base-types/implementations/NumericQuestion')
+const ShortAnswerQuestion = require('../model/base-types/implementations/ShortAnswerQuestion')
+const TrueFalseQuestion = require("../model/base-types/implementations/TrueFalseQuestion");
 
 /**
  * Contrôleur des questions, gère les opérations portant sur celle-ci
@@ -28,6 +31,42 @@ class QuestionController {
      */
     createMultipleChoice(question, answerSet, correctAnswers) {
         let newQuestion = new MultipleChoiceQuestion(question, answerSet, correctAnswers);
+        this.#addToCache(newQuestion);
+        return newQuestion;
+    }
+
+    /**
+     * Créée une nouvelle question numérique
+     * @param question {string} - Enoncé de la question
+     * @param answer {number} - Réponse valide
+     * @return {NumericQuestion} - Question créée
+     */
+    createNumeric(question, answer) {
+        let newQuestion = new NumericQuestion(question, answer);
+        this.#addToCache(newQuestion);
+        return newQuestion;
+    }
+
+    /**
+     * Créée une nouvelle question à réponse courte
+     * @param question {string} - Enoncé de la question
+     * @param answer - Réponse attendue
+     * @returns {ShortAnswerQuestion} - Question créée
+     */
+    createShortAnswer(question, answer) {
+        let newQuestion = new ShortAnswerQuestion(question, answer);
+        this.#addToCache(newQuestion);
+        return newQuestion;
+    }
+
+    /**
+     * Créée une question vrai/faux
+     * @param question {string} - Enoncé de la question
+     * @param answer {boolean} - Réponse valide
+     * @returns {TrueFalseQuestion} - Question instanciée
+     */
+    createTrueFalse(question, answer) {
+        let newQuestion = new TrueFalseQuestion(question, answer);
         this.#addToCache(newQuestion);
         return newQuestion;
     }
