abf5f444d2e7a1e6b2d8d07028a84e84caf436c3
NathanHonoré <154017011+Phirix62@users.noreply.github.com>
2024-12-08 01:55:14 +0100

    début command simultest

--- app/view/TestCommands.js
+++ app/view/TestCommands.js
@@ -1,7 +1,8 @@
 const TestController = require('../controller/TestController');
 const Test = require('../model/base-types/Test');
-const {prettyTestList} = require('./pretty-printing-tools/TestPrinter');
+const { prettyTestList } = require('./pretty-printing-tools/TestPrinter');
 const QuestionCache = require('../controller/utils/QuestionCache');
+const inquirer = require('inquirer').default;
 
 function addTestCommands(program) {
 
@@ -13,7 +14,7 @@ function addTestCommands(program) {
         .action((questionsIds = []) => {
             const test = controller.createTest();
             questionsIds.forEach(id => {
-                const question = QuestionCache.instance.getQuestion(id);
+                const question = QuestionCache.instance.getQuestion(parseInt(id));
                 if (question) {
                     controller.addQuestionToTest(test, question);
                 } else {
@@ -28,7 +29,7 @@ function addTestCommands(program) {
             let tests = controller.readAll();
             console.log(prettyTestList(tests));
         })
-        program
+    program
         .command('rmtest')
         .description("Delete a test")
         .argument('<id>', 'The id of the test to delete')
@@ -41,7 +42,7 @@ function addTestCommands(program) {
                 console.log(`Test with id ${id} not found.`);
             }
         })
-        program
+    program
         .command('editest')
         .description("Edit a test")
         .argument('<id>', 'The id of the test to edit')
@@ -74,7 +75,7 @@ function addTestCommands(program) {
                 }
             }
         })
-        program
+    program
         .command('veriftest') // en attente de la creation vCard
         .description("Verify that a test is valid")
         .argument('<id>', 'The id of the test to verify')
@@ -91,8 +92,47 @@ function addTestCommands(program) {
                 console.log(`Test with id ${id} is not valid.`);
             }
         })
-        
-        
+
+    program
+        .command('simultest')
+        .description("Simulate a verified test")
+        .argument('<id>', 'The id of the test to simulate')
+        .action(async (id) => {
+            const test = controller.readAll().find(t => t.id === parseInt(id));
+            if (!test) {
+                console.log(`Test with id ${id} not found.`);
+                return;
+            }
+            //check la validité du test
+            let score = 0;
+            const correctQuestions = [];
+            const incorrectQuestions = [];
+
+            for (const question of test.questions) {
+                const answer = await inquirer.prompt([{
+                    type: 'input',
+                    name: 'userAnswer',
+                    message: question.question
+                }]);
+
+                if (answer.userAnswer === question.answer) {
+                    score++;
+                    correctQuestions.push(question.question);
+                } else {
+                    incorrectQuestions.push(question.question);
+                }
+            }
+
+            console.log(`Your score: ${score}/${test.questions.length}`);
+            console.log('Correct questions:');
+            correctQuestions.forEach(q => console.log(q));
+            console.log('Incorrect questions:');
+            incorrectQuestions.forEach(q => console.log(q));
+
+        })
+
+
+
 
 }
 module.exports = addTestCommands;
