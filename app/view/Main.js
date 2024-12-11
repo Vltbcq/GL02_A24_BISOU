f4b341d9aaa817c3c531eb008439932e57ac9e8c
NathanHonoré <nathan.honore@outlook.fr>
2024-12-11 22:27:02 +0100

    petit ajout commande readme

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,13 +1,30 @@
 const { program } = require('commander');
 const addQuestionCommands = require('./QuestionCommands');
 const addTestCommands = require('./TestCommands');
+const fs = require('fs');
 
 program
     .name("SRU Tests Manager by Bisou")
     .description("A brand new software for managing tests and questions at Sealand's Republic University")
 
+program
+    .command('readme')
+    .description('Display the readme file')
+    .action(() => {
+        fs.readFile("README.md", 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(data);
+        });
+    });
+
 addQuestionCommands(program);
 addTestCommands(program);
+
+//command to display readme file
+
 
 program.parse(process.argv);
 
