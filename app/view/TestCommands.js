ca9506e7cc5f4bd3011b84c2580dddd41847abbc
AdelineVercoutre <adeline.vercoutre@utt.fr>
2024-12-12 14:48:30 +0100

    on retire veriftest

--- app/view/TestCommands.js
+++ app/view/TestCommands.js
@@ -51,61 +51,43 @@ function addTestCommands(program) {
                 console.log(`Test with id ${id} not found.`);
             }
         })
-        program
-            .command('editest')
-            .description("Edit a test")
-            .argument('<id>', 'The id of the test to edit')
-            .argument('<questionId>', 'The id of the question to add or remove')
-            .action(async (id, questionId) => {
-                const test = TestCache.instance.getTestById(parseInt(id));
-                if (!test) {
-                    console.log(`Test with id ${id} not found.`);
-                    return;
-                }
-
-                const { action } = await inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'action',
-                        message: 'Do you want to add or remove the question?',
-                        choices: ['Add', 'Remove']
-                    }
-                ]);
-
-                const question = QuestionCache.instance.getQuestion(parseInt(questionId));
-                if (!question) {
-                    console.log(`Question with id ${questionId} not found.`);
-                    return;
-                }
-
-                if (action === 'Add') {
-                    controller.addQuestionToTest(test, question);
-                    console.log(`Question with id ${questionId} added to test ${id}.`);
-                } else if (action === 'Remove') {
-                    controller.removeQuestionFromTest(test, question);
-                    console.log(`Question with id ${questionId} removed from test ${id}.`);
-                }
-            })
-        
     program
-        .command('veriftest') // en attente de la creation vCard
-        .description("Verify that a test is valid")
-        .argument('<id>', 'The id of the test to verify')
-        //.argument('<vCardFile>', 'The vCard file needed the verification')
-        .action((id) => {
-            const test = controller.readAll().find(t => t.id === parseInt(id));
+        .command('editest')
+        .description("Edit a test")
+        .argument('<id>', 'The id of the test to edit')
+        .argument('<questionId>', 'The id of the question to add or remove')
+        .action(async (id, questionId) => {
+            const test = TestCache.instance.getTestById(parseInt(id));
             if (!test) {
                 console.log(`Test with id ${id} not found.`);
                 return;
             }
-            if (test.isValid()) {
-                console.log(`Test with id ${id} is valid.`);
-            } else {
-                console.log(`Test with id ${id} is not valid.`);
+
+            const { action } = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'Do you want to add or remove the question?',
+                    choices: ['Add', 'Remove']
+                }
+            ]);
+
+            const question = QuestionCache.instance.getQuestion(parseInt(questionId));
+            if (!question) {
+                console.log(`Question with id ${questionId} not found.`);
+                return;
+            }
+
+            if (action === 'Add') {
+                controller.addQuestionToTest(test, question);
+                console.log(`Question with id ${questionId} added to test ${id}.`);
+            } else if (action === 'Remove') {
+                controller.removeQuestionFromTest(test, question);
+                console.log(`Question with id ${questionId} removed from test ${id}.`);
             }
         })
-
-        program
+        
+    program
         .command('simultest')
         .description("Simulate a verified test")
         .argument('<id>', 'The id of the test to simulate')
@@ -215,10 +197,5 @@ function addTestCommands(program) {
                 console.log(data);
             });
         });
-        
-
-
-
-
 }
 module.exports = addTestCommands;
